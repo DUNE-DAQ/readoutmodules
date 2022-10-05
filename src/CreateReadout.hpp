1e336ad63a4a53c13b48be46c40dbd39d5464f42
Ivana Radoslavova Hristova <ivana.hristova@cern.ch>
2022-10-05 12:14:10 +0100

    Make FW TP processor accessible via wib2

--- src/CreateReadout.hpp
+++ src/CreateReadout.hpp
@@ -27,7 +27,7 @@
 #include "fdreadoutlibs/daphne/DAPHNEFrameProcessor.hpp"
 #include "fdreadoutlibs/daphne/DAPHNEListRequestHandler.hpp"
 #include "fdreadoutlibs/ssp/SSPFrameProcessor.hpp"
-#include "fdreadoutlibs/wib/RAWWIBTriggerPrimitiveProcessor.hpp"
+#include "fdreadoutlibs/wib2/RAWWIBTriggerPrimitiveProcessor.hpp"
 #include "fdreadoutlibs/wib/SWWIBTriggerPrimitiveProcessor.hpp"
 #include "fdreadoutlibs/wib/WIBFrameProcessor.hpp"
 #include "fdreadoutlibs/wib2/WIB2FrameProcessor.hpp"
